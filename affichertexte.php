647cbd4e4c73bdab76c3c976f90651d2ea059536
Antoine MARTIN <antoine.martin2023@campus-eni.fr>
2023-07-26 15:20:27 +0200

    PHP

---
+++ affichertexte.php
@@ -0,0 +1,166 @@
+<?php echo "Ceci est du texte en PHP"; ?>
+
+<!-- Ou bien, avec des parenthèses -->
+<?php echo("Ceci est du <strong>texte</strong>"); ?>
+
+<?php echo "Cette ligne a été écrite \"uniquement\" en PHP."; ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+    </body>
+</html>
+
+<?php
+echo "J'habite en Chine."; // Cette ligne indique où j'habite
+
+// La ligne suivante indique mon âge
+echo "J'ai 92 ans.";
+?>
+
+<?php
+/* La ligne suivante indique mon âge
+Si vous ne me croyez pas...
+... vous avez raison ;o) */
+echo "J'ai 92 ans.";
+?>
+
+<?php
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => '',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => '',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => '',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Salade Romaine',
+        'recipe' => '',
+        'author' => 'laurene.castor@exemple.com',
+        'is_enabled' => false,
+    ],
+];
+
+foreach($recipes as $recipe) {
+    echo $recipe['title'] . ' contribué(e) par : ' . $recipe['author'] . PHP_EOL; 
+}
+?>
+
+<?php
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => '',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => '',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+];
+
+echo '<pre>';
+print_r($recipes);
+echo '</pre>';
+?>
+
+<?php
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Affichage des recettes</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+</head>
+<body>
+    <div class="container">
+        <h1>Affichage des recettes</h1>
+        <!-- Boucle sur les recettes -->
+        <?php foreach($recipes as $recipe) : ?>
+            <!-- si la clé existe et a pour valeur "vrai", on affiche -->
+            <?php if (array_key_exists('is_enabled', $recipe) && $recipe['is_enabled'] == true): ?>
+
+                <article>
+                    <h3><?php echo $recipe['title']; ?></h3>
+                    <div><?php echo $recipe['recipe']; ?></div>
+                    <i><?php echo $recipe['author']; ?></i>
+                </article>
+
+            <?php endif; ?>
+        <?php endforeach ?>
+    </div>   
+</body>
+</html>
+
+
+<?php
+$recipe = 'Etape 1 : des flageolets ! Etape 2 : de la saucisse toulousaine';
+$length = strlen($recipe);
+ 
+ 
+echo 'La phrase ci-dessous comporte ' . $length . ' caractères :' . PHP_EOL . $recipe;
+?>
+<?php
+echo str_replace('c', 'C', 'le cassoulet, c\'est très bon');
+?>
+
+<?php
+// Enregistrons les informations de date dans des variables
+
+$day = date('d');
+$month = date('m');
+$year = date('Y');
+
+$hour = date('H');
+$minut = date('i');
+
+// Maintenant on peut afficher ce qu'on a recueilli
+echo 'Bonjour ! Nous sommes le ' . $day . '/' . $month . '/' . $year . 'et il est ' . $hour. ' h ' . $minut;
+?>
